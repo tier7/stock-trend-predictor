c3aa75ac381d6e402cf90c44d7bc89ed18d49ff4
tier7 <91539085+tier7@users.noreply.github.com>
2026-07-07 19:28:28 +0200

    Added stock price retrieval from database

--- services/db.py
+++ services/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from data_fetcher import *
+import pandas as pd
 
 DB_NAME = "stocks.db"
 
@@ -48,8 +49,22 @@ def save_stock_data(data):
     conn.commit()
     cur.close()
 
+def get_stock_data(ticker, interval="1d", years=5):
+    conn = get_connection()
+    ticker = ticker.upper()
+
+    if years is None:
+        query = "SELECT * FROM stock_prices WHERE ticker = ? AND interval = ? ORDER BY date ASC"
+    else:
+        query = "SELECT * FROM stock_prices WHERE ticker = ? AND interval = ? AND date >= date('now', ?) ORDER BY date ASC"
+    data = pd.read_sql_query(query, conn, params=(ticker, interval, f"-{years} years"))
+
+    conn.close()
+    return data
+
 if __name__ == "__main__":
     create_table()
     print("Table stock_prices created")
     data = fetch_stock_data("AAPL")
     save_stock_data(data)
+
