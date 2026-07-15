f719c620223252430731591efa0dbcd3b9655fd8
tier7 <wiktor.zieba333@hotmail.com>
2026-07-16 01:11:45 +0200

    Added relative moving average features to model

--- services/ml/dataset_service.py
+++ services/ml/dataset_service.py
@@ -5,6 +5,10 @@ FEATURE_COLUMNS = [
     "sma_26",
     "ema_12",
     "ema_26",
+    "price_vs_sma_12",
+    "price_vs_sma_26",
+    "price_vs_ema_12",
+    "price_vs_ema_26",
     "macd",
     "macd_signal",
     "rsi_14",
@@ -20,6 +24,11 @@ def prepare_dataset(data):
     data = data.copy()
     data = data.sort_values("date")
     data = calculate_all_indicators(data)
+
+    data["price_vs_sma_12"] = data["close"] / data["sma_12"] - 1
+    data["price_vs_sma_26"] = data["close"] / data["sma_26"] - 1
+    data["price_vs_ema_12"] = data["close"] / data["ema_12"] - 1
+    data["price_vs_ema_26"] = data["close"] / data["ema_26"] - 1
 
     data["next_close"] = data["close"].shift(-1)
     data["target"] = (data["next_close"] > data["close"]).astype(int)
